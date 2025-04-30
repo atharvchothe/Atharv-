6f731e3a3c8ece150b457a46f7e9d91f21899cf2
atharvchothe <atharvchothe839@gmail.com>
2025-04-30 20:52:28 +0530

    Add files via upload

---
+++ class array .cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<iomanip>
+using namespace std ;
+class person {
+	private :
+		char name[20];
+		char city [20];
+		char gender [6];
+		char uid[12];
+	public :
+		void setinfo();
+		void dispinfo();
+};
+  void person :: setinfo()
+  {
+  	cout<<"\n the name of the person :";
+  	cin>>name;
+  	cout<<"\n the city of person :   ";
+  	cin>>city;
+  	cout<<"\n gender :           :    ";
+  	cin>>gender ;
+  	cout<<"\n the uid no of person :  ";
+  	cin>>uid ;
+  }
+  void person :: dispinfo()
+  {
+  	cout<<"\n  the name of [person :    "<<name ;
+  	cout<<"\n the city of person :     "<<city ;
+  	cout<<"\n the gender         :     "<<gender ;
+  	cout<<"\n the uid no of person :   "<<uid ;
+  }
+  
+  /*class student : public person  
+{
+	private : 
+		int no 	,s1,s2,tot ;
+	public : 
+		void setstudinfo();
+		void dispstudinfo();
+};*/
+int main()
+{ 
+  /*person p1;
+   p1.setinfo();
+   p1.dispinfo();
+   
+   person p2;
+   p2.setinfo();
+   p2.dispinfo(); */
+    
+  int   i;
+    obj1.[5];
+   for (i=0 ; i<4; i++)
+   {
+   	 obj[i].setinfo()
+   	 
+   }
+   for (i=0 ; i<4; i++)
+   {
+   	 obj[i].dispnfo()
+   	 
+   }
+}
+
